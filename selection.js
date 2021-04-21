08a5d0c0fdca58933e075b170a5e2d2ad6536a21
Aaron Brant <72094000+KortesmakiAB@users.noreply.github.com>
2021-04-21 16:55:54 -0400

    added selection sort.

--- selection.js
+++ selection.js
@@ -1,3 +1,20 @@
-function selectionSort() {}
+function selectionSort(arr) {
+    debugger    
+    for (let i = 0; i < arr.length; i++) {
+        let smallestValIdx = i;
+
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[smallestValIdx]) smallestValIdx = j;
+        }
+        
+        if (smallestValIdx !== i) {
+            const tempVal = arr[i];
+            arr[i] = arr[smallestValIdx];
+            arr[smallestValIdx] = tempVal;
+        }
+    }
+    
+    return arr;
+}
 
 module.exports = selectionSort;
